bf88a32a121f3c182711df947797332b724d5d3e
sammy-the-sea-lion <60279097+sammy-the-sea-lion@users.noreply.github.com>
2020-02-06 19:20:28 -0500

    Added Addition [TestCase] fails. Needs equlaity operator.

--- HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld.Tests/UnitTest1.cs
@@ -14,23 +14,39 @@ namespace HelloWorld.Tests
         public void Two_Plus_Two_Equals_Four()
         {
             var two = new UnaryExpression<int>(2);
-            var additionExpression = new AdditionExpression(two,two);
+            var additionExpression = new AdditionExpression(two, two);
             var value = additionExpression.Value;
-            
-            Assert.AreEqual(4,value,"Two + Two = 4");
 
-        }  [Test]
-        public void Two_Plus_Two_Times_Negative_Four_Equals_negative_Eight()
-        {
-            var two = new UnaryExpression<int>(2);
-            var additionExpression = new AdditionExpression(two,two);
-            var multiply = new Multiply(additionExpression, new NegativeOf(two.Value));
-            var value = additionExpression.Value;
-            
-            Assert.AreEqual(-8,value,"Two + Two = 4");
+            Assert.AreEqual(4, value, "Two + Two = 4");
 
         }
 
-       
+        [TestCase(2, 2, 4)]
+        public void Addition_TestCases(int a, int b, int expectedValue)
+        {
+
+            var additionExpression = new AdditionExpression(new UnaryExpression<int>(a), new UnaryExpression<int>(b));
+
+            var value = additionExpression;
+
+
+            Assert.AreEqual(new UnaryExpression<int>(expectedValue), value);
+
+        }
+
+
+        [Test]
+        public void Two_Plus_Two_Times_Negative_Four_Equals_negative_Eight()
+        {
+            var two = new UnaryExpression<int>(2);
+            var additionExpression = new AdditionExpression(two, two);
+            var multiply = new Multiply(additionExpression, new NegativeOf(two.Value));
+            var value = additionExpression.Value;
+
+            Assert.AreEqual(-8, value, "Two + Two = 4");
+
+        }
+
+
     }
 }
