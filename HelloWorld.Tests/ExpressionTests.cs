348e28903591a66db3773d76ef260895cd124b8a
sammy-the-sea-lion <60279097+sammy-the-sea-lion@users.noreply.github.com>
2020-02-07 18:54:28 -0500

    Finished Testcases for Expressions

--- HelloWorld.Tests/ExpressionTests.cs
+++ HelloWorld.Tests/ExpressionTests.cs
@@ -22,6 +22,8 @@ namespace HelloWorld.Tests
         }
 
         [TestCase(2, 2, 4)]
+        [TestCase(2, 0, 2)]
+        [TestCase(0, -2, -2)]
         public void Addition_TestCases(int a, int b, int expectedValue)
         {
 
@@ -41,7 +43,7 @@ namespace HelloWorld.Tests
             var two = new UnaryExpression<int>(2);
             var additionExpression = new AdditionExpression(two, two);
             var multiply = new Multiply(additionExpression, new NegativeOf(two.Value));
-            var value = additionExpression.Value;
+            var value = multiply.Value;
 
             Assert.AreEqual(-8, value, "Two + Two = 4");
 
